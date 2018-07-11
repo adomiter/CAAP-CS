b7744511c3c940a5ec0fa23bcc14f8b25dcc6c50
U-DESKTOP-HHB7Q7K\Amanda <adomiter@uchicago.edu>
2018-07-10 23:27:18 -0500

    Created cash.py and fibonacci_seq.py

---
+++ hw1/cash.py
@@ -0,0 +1,12 @@
+#this program calculates the minimum number of coins required to give a user change
+def change():
+    remain=eval(input("What is the total in dollars? "))
+    remain *= 100
+    total=0
+    dollars = (100, 50, 20, 10, 5, 1, 0.25, 0.10, 0.05, 0.01)
+    for i in dollars:
+        change, remain = divmod(remain, i*100)
+        print("$%s: %s" % (i,int(change)))
+        total += change
+    print("The total number of coins is %s." % (int(total)))
+change()
--- hw1/fibonacci_seq.py
+++ hw1/fibonacci_seq.py
@@ -1,7 +1,14 @@
 #computes the nth Fibonacci number where n is a value input by the user 
-def fib():
-    nth_term=eval(input("What is the value of n where n is the nth Fibonacci number?"))
-    fib_number=
-    print("If n=%s then the result is %s." % (nth_term, fib_number)) 
+def fib1(n):
+    if (n==0):
+        return 0
+    elif (n==1):
+        return 1
+    else:
+        return fib1(n-1)+fib1(n-2)
     
-fib()
+
+def main():
+    nth_term=eval(input("What is the value of n where n is the nth Fibonacci number? "))
+    print("If n= %s then the result is %s." % (nth_term, fib1(nth_term))) 
+main()    
